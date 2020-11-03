4129c82332bd1f348129e8014939d75ea16b2f16
kheangp <72509101+kheangp@users.noreply.github.com>
2020-11-03 15:42:46 +0100

    Add files via upload

---
+++ api/checkemail.php
@@ -0,0 +1,33 @@
+<?php
+
+	include "../includes/database.php";
+	/*include "../security/secure.php";*/
+	
+	
+	@$email=$_GET["mail"];
+ 
+ // verifier si email existe dans table USER
+	$sql = "SELECT email
+				FROM user 
+				WHERE email='$email'";
+
+	$sth = $dbco->prepare($sql);
+
+	$sth->execute();
+	$resultat = $sth->fetch(PDO::FETCH_ASSOC);
+	
+	if($sth->rowCount()>0)
+	{
+		echo "KO"; // l'utilisateur existe déjà dans BD
+		
+		
+	}
+	else
+	{
+		echo "OK"; // l'utilisateur n'existe pas 
+		
+	}
+ 
+
+
+
---
+++ api/validatepassword.php
@@ -0,0 +1,54 @@
+
+<?php
+
+include "../includes/database.php";
+
+	$password=$_GET["password"];
+
+
+	if(strlen($password)>8)
+	{
+		echo "valid"; // mot de passe superieur à 8 caractères
+				
+	}
+	else
+	{
+		echo "unvalid"; 
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+ 
+ /* verifier si email existe dans table USER
+	$sql = "SELECT  password
+				FROM user 
+				WHERE password='$password'";
+
+	$sth = $dbco->prepare($sql);
+
+	$sth->execute();
+	$resultat = $sth->fetch(PDO::FETCH_ASSOC);
+	
+	if($sth->rowCount()>0)
+	{
+		echo "valid"; //
+		
+	}
+	else
+	{
+		echo "unvalid"; 
+		
+	}
+*/
+
+
+
+
